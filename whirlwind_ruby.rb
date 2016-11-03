e6cd9ff136ef0a9e79e8362f95948c5b3fe0d852
meg-murphy <meghan.murphy.usc@gmail.com>
2016-11-02 21:01:21 -0700

    added

--- whirlwind_ruby.rb
+++ whirlwind_ruby.rb
@@ -1,30 +1,68 @@
-#1_a
-favorite_foods = []
-  5.times do
-    puts "What is your favorite food?"
-    food = gets.chomp
-    favorite_foods << food
-  end
-p favorite_foods
+# #1_a
+# favorite_foods = []
+#   5.times do
+#     puts "What is your favorite food?"
+#     food = gets.chomp
+#     favorite_foods << food
+#   end
+# p favorite_foods
+#
+# #1_b
+# favorite_foods = []
+#   5.times do
+#     puts "What is your favorite food?"
+#     food = gets.chomp
+#     x = "I love #{food}"
+#     favorite_foods << x
+#   end
+# puts favorite_foods
+#
+# #1_c
+# counter = 1
+# favorite_foods = []
+#   5.times do
+#     puts "What is your favorite food?"
+#     food = gets.chomp
+#     x = "#{counter}. #{food}"
+#     favorite_foods << x
+#     counter += 1
+#   end
+# puts favorite_foods
 
-#1_b
-favorite_foods = []
-  5.times do
-    puts "What is your favorite food?"
-    food = gets.chomp
-    x = "I love #{food}"
-    favorite_foods << x
-  end
-puts favorite_foods
+# #
+# counter = -1
+# 11.times do
+#   counter += 1
+#   puts counter
+# end
+#
 
-#1_c
-counter = 1
-favorite_foods = []
-  5.times do
-    puts "What is your favorite food?"
-    food = gets.chomp
-    x = "#{counter}. #{food}"
-    favorite_foods << x
-    counter += 1
-  end
-puts favorite_foods
+# result = false
+#
+# if result != true
+#   puts "result"
+# else
+#   puts "no"
+# end
+#
+# unless result == true
+#   puts "result"
+# end
+#Yes, code runs the same
+
+
+
+
+# result = (5 + 5)
+# numbers = [1, 2, 3, 4, 5]
+# numbers.each do |number|
+#   x = number + 5
+#   unless result == x
+#     puts "Hello!"
+#   end
+#
+# end
+result = false
+unless result
+  puts "Hello!"
+end
